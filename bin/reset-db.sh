63b78032f9bab2578f41bafc828fa3ad548246b4
YoshiBleu9427 <nicolas118218@msn.com>
2016-10-28 14:33:37 +0200

    Create User entity, datafixture, update composer, create reset-db script

---
+++ bin/reset-db.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+bin/console doctrine:schema:drop --force
+bin/console doctrine:schema:create
+bin/console doctrine:fixtures:load --append
---
+++ src/AppBundle/DataFixtures/ORM/UserFixture.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\User;
+
+class UserFixture extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $userAdmin = new User();
+        $userAdmin->setUsername('admin');
+        $userAdmin->setPassword('test');
+        $userAdmin->setEmail('admin@admin.com');
+
+        $manager->persist($userAdmin);
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        // the order in which fixtures will be loaded
+        // the lower the number, the sooner that this fixture is loaded
+        return 1;
+    }
+}
