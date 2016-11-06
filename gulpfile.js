4ec52d3d870e95611bcf2c31b3685459cd85547b
YoshiBleu9427 <nicolas118218@msn.com>
2016-11-06 21:16:58 +0100

    New index page; Install Bootstrap

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var copy = require('gulp-copy');
+
+
+gulp.task('default', function () {});
+
+gulp.task('sass', function () {
+    gulp.src('./web/bundles/app/sass/master.scss')
+        .pipe(sass({
+            sourceComments: 'map',
+            includePaths: [
+                "./web",
+                "./web/components",
+                "./web/components/bootstrap-sass/assets/stylesheets",
+            ]
+        }))
+        .pipe(gulp.dest('./web/css/'));
+});
+
+gulp.task('fonts', function () {
+    return gulp.src('./web/components/bootstrap-sass-official/assets/fonts/bootstrap/*')
+        .pipe(copy('./web/fonts', {prefix: 7}));
+});
